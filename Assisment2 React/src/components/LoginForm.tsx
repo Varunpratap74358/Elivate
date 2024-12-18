e59d078e08e89b09d37e16c46a304a63db7519fc
Varin Singh <varunpratap74358@gmail.com>
2024-12-19 00:10:10 +0900

    Responsive complited

--- Assisment2 React/src/components/LoginForm.tsx
+++ Assisment2 React/src/components/LoginForm.tsx
@@ -1,47 +1,68 @@
-import { FormEvent, useState } from 'react'
-import { BiCartDownload } from 'react-icons/bi'
-import { FaRegUser } from 'react-icons/fa'
-import { RiLockPasswordLine } from 'react-icons/ri'
+import { FormEvent, useState } from "react";
+import { BiCartDownload } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const LoginForm = () => {
-    const [username,setUsername] = useState<string>("")
-    const [password,setPassword] = useState<string>("")
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    const handelSubmit=(e:FormEvent)=>{
-        e.preventDefault()
-        console.log(username,password)
-        alert(username+" " + password)
-
-    }
+  const handelSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(username, password);
+    alert(username + " " + password);
+  };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-700">
+    <div className="flex items-center justify-center min-h-screen bg-blue-700 px-3">
       <div className="relative w-full max-w-md p-6 bg-blue-700 rounded-lg border-blue-600 border shadow-md flex flex-col gap-7">
         <div className="text-white flex justify-center">
-        <BiCartDownload size={75} />
+          <BiCartDownload size={75} />
         </div>
-        <form onSubmit={handelSubmit} action="" className='flex flex-col gap-3'>
-            <div>
-                <div className="text-white bg-blue-700 border flex items-center rounded-md py-2 px-3">
-                <FaRegUser size={25} />
-                <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} className='bg-blue-700 w-full text-xl px-4 focus:outline-none ' placeholder='USERNAME' />
-                </div>
+        <form onSubmit={handelSubmit} autoComplete="off" className="flex flex-col gap-3">
+          <div>
+            <div className="text-white bg-blue-700 border flex items-center rounded-md py-2 px-3">
+              <FaRegUser size={25} />
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="bg-blue-700 w-full text-md md:text-xl px-4 focus:outline-none "
+                placeholder="USERNAME"
+                autoComplete="off"
+              />
             </div>
-            <div>
-                <div className="text-white bg-blue-700 border flex items-center rounded-md py-2 px-3">
-                <RiLockPasswordLine size={25} />
-                <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className='bg-blue-700 w-full text-xl px-4 focus:outline-none ' placeholder='PASSWORD' />
-                </div>
+          </div>
+          <div>
+            <div className="text-white bg-blue-700 border flex items-center rounded-md py-2 px-3">
+              <RiLockPasswordLine size={25} />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="bg-blue-700 w-full text-md md:text-xl px-4 focus:outline-none "
+                placeholder="PASSWORD"
+                autoComplete="off"
+              />
             </div>
-            <div>
-                <button type='submit' className='bg-white hover:bg-slate-100 transition-all duration-300 w-full py-2 rounded mt-10 font-semibold'>Login</button>
-            </div>
-            <div className='flex justify-end'>
-                <a href="" className='text-white'>Forgot password?</a>
-            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-white hover:bg-slate-100 transition-all duration-300 w-full py-2 rounded mt-10 font-semibold"
+            >
+              Login
+            </button>
+          </div>
+          <div className="flex justify-end">
+            <a href="#" className="text-white">
+              Forgot password?
+            </a>
+          </div>
         </form>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
